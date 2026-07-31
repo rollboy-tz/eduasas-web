81c9800c46094e8a35b54cc64b54c4d1834dafa7
rollboy-tz <rollboytz1@gmail.com>
2026-07-31 14:28:55 +0300

    Commit push runtime correction

--- proxy.ts
+++ proxy.ts
@@ -33,5 +33,4 @@ export const config = {
   matcher: [
     "/((?!api|main|_next/static|_next/image|icons|images|fonts|favicon.ico|robots.txt|sitemap.xml|manifest.json|sw.js|workbox-).*)",
   ],
-  runtime:"edge",
 };
